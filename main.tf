9a819348cd60ec3ce86adc78aedefef214ec44cf
Velupula Hinduja <hinduja.velupula@thoughtworks.com>
2023-04-19 12:31:57 +0530

    Refactor. format the code

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 module "to-do-app-cluster" {
   source = "./module-eks"
 }
+
+resource "aws_s3_bucket" "infra-todo-s3" {
+  bucket = "infra-todo"
+}
+
+module "my-vpc" {
+  source = "./module-vpc"
+}
--- s3.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "infra-todo-s3" {
-  bucket = "infra-todo"
-}
