652242e537a19fd6c2986e7d0cc3d8faea1f39cf
Velupula Hinduja <hinduja.velupula@thoughtworks.com>
2023-04-19 17:22:38 +0530

    Refactor. change availability zone

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
-
-
 module "my-vpc" {
   source = "./module-vpc"
 }
 
-module "to-do-app-cluster" {
-  source = "./module-eks"
-}
+# module "to-do-app-cluster" {
+#   source = "./module-eks"
+# }
 
-module "to-do-app-k8s" {
-  source = "./module-k8s"
-}
+# module "to-do-app-k8s" {
+#   source = "./module-k8s"
+# }
 
 
 
