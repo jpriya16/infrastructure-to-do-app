ba764a5484174b8eebf677f42a4e7193a990f641
Velupula Hinduja <hinduja.velupula@thoughtworks.com>
2023-04-19 18:45:36 +0530

    Refactor. format file

--- main.tf
+++ main.tf
@@ -2,13 +2,10 @@ module "my-vpc" {
   source = "./module-vpc"
 }
 
-# module "to-do-app-cluster" {
-#   source = "./module-eks"
-# }
+module "to-do-app-cluster" {
+  source = "./module-eks"
+}
 
-# module "to-do-app-k8s" {
-#   source = "./module-k8s"
-# }
-
-
-
+module "to-do-app-k8s" {
+  source = "./module-k8s"
+}
