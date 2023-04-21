a0e564cb594b03342600f84e3be9dea808fbda19
Velupula Hinduja <hinduja.velupula@thoughtworks.com>
2023-04-21 12:53:24 +0530

    Add. eks cluster

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ module "my-vpc" {
   source = "./module-vpc"
 }
 
-# module "to-do-app-cluster" {
-#   source = "./module-eks"
-# }
+module "to-do-app-cluster" {
+  source = "./module-eks"
+}
 
 # module "to-do-app-k8s" {
 #   source = "./module-k8s"
